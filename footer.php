8d425dbd44941f34adb493ff89a0614d063bd14c
Sandro Griebeler <sandrogriebeler@gmail.com>
2012-03-30 12:27:36 -0300

    a

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
             <div id="footer" style="position: relative;">                
                 <span style="position: absolute;bottom:10px;right:10px;">
-                    <a href="http://www.ggsd.com.br" target="_blank">GGSD - Web Solutions</a>
+                    <a href="http://www.ggsd.com.br" target="_blank" title="GGSD - Web Solutions">   
+                        <img src="./img/minilogo.png" alt="ggsd" />
+                    </a>                    
                 </span>
+                
+                <div id="contCredito">© 2012 NonnaAnna</div>
             </div>
         </div>
     </body>
--- header.php
+++ header.php
@@ -3,6 +3,9 @@
     <head>
         <title>Karin</title>
         <link rel="stylesheet" type="text/css" href="./css/screen.css"/>
+        <link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre:300italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="./plugins/fancybox/jquery.fancybox-1.3.4.css"/>        
         <script type="text/javascript" src="./js/jquery-1.7.2.min.js"></script>
         <script type="text/javascript" src="./js/plugins/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
@@ -10,6 +13,7 @@
         <script type="text/javascript" src="./js/plugins/fancybox/jquery.easing-1.3.pack.js"></script>
     </head>
     <body>
+    <div id="shadow">
         <div id="site"> 
             <div id="header">
                 <a href="./index.php" id="logo">
@@ -17,7 +21,8 @@
                 </a>
             </div>
             
-            <div id="lateral">            
+            <div id="lateral">
+            
                 <div id="menu">
                     <ul>
                         <li><a href="./index.php">HOME</a></li>
@@ -28,6 +33,8 @@
                     </ul>    
                 </div>
                 
+                
+                
                 <div id="noticias">
                     <h2>Not&iacute;cias r&aacute;pidas</h2>
                     
@@ -36,7 +43,7 @@
                     <? for($i = 0; $i < 4; $i++){ ?>
                     
                     <a href="./noticias.php?cod=<? echo $i+1;//$result[$i]["cod_noticia"] ?>">
-                        <strong>24/03/2012</strong> 
+                        <strong>24/03/2012</strong>
                         <p id="estiloNoticia">Mustang ridículo foi construído pela Microsoft.</p>
                     </a>                
                     <br /><br />
@@ -44,5 +51,5 @@
                     <? } ?>                                        
                 </div>
             </div>
-            
+         
     
